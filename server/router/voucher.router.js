07367b84f680482095a190f82d941d44c89ee70b
JOHN <johnregulacion5555@gmail.com>
2024-05-26 14:20:43 +0800

    dfg

--- server/router/voucher.router.js
+++ server/router/voucher.router.js
@@ -1,9 +1,11 @@
-const express= require('express')
-const router= express.Router
-const {createVoucher,listVoucher}= require('./../controller/voucher.controller')
+const express = require("express");
+const router = express.Router();
+const {
+  createVoucher,
+  listVoucher,
+} = require("./../controller/voucher.controller");
 
-
-router.post('/createVoucher',createVoucher)
-router.get('/listVoucher',listVoucher)
+router.post("/createVoucher", createVoucher);
+router.get("/listVoucher", listVoucher);
 
 module.exports = router;
